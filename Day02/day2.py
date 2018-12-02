c226c2172d5657e628e8f6088373f115cc1e17f2
will Ediger <will.ediger@gmail.com>
2018-12-02 14:32:23 -0600

    day2 part2 solution complete

--- Day02/day2.py
+++ Day02/day2.py
@@ -16,3 +16,20 @@ for i in range(0, len(file_array)):
 
 pt1answer = count_of_twos * count_of_threes
 print(pt1answer)
+
+file_array_item_len = len(file_array[0])
+file_array_len = len(file_array)
+for i in range(0, file_array_len):
+    for j in range(0, file_array_len):
+        if i < j:
+            letter_diffs = 0
+            for letter_idx in range(0, file_array_item_len):
+                if file_array[i][letter_idx] != file_array[j][letter_idx]:
+                    letter_diffs += 1
+            if letter_diffs == 1:
+                solution_string = ''
+                for letter_idx in range(0, file_array_item_len):
+                    if file_array[i][letter_idx] == file_array[j][letter_idx]:
+                        solution_string += file_array[i][letter_idx]
+
+print(solution_string)
