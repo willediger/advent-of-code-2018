a9a04c1e0e7686070c5c2283e7c0891465ef9c33
will Ediger <will.ediger@gmail.com>
2018-12-02 14:13:33 -0600

    part 1 answer complete

--- Day02/day2.py
+++ Day02/day2.py
@@ -1,11 +1,18 @@
 from functools import reduce
 from collections import Counter
 
-file_input = open('Day02/test_input', 'r')
+file_input = open('Day02/input', 'r')
 file_array = file_input.read().splitlines()
 file_input.close()
 
+count_of_twos = 0
+count_of_threes = 0
 for i in range(0, len(file_array)):
-    x = Counter(file_array[i])
-    y = type(x)
-    print(Counter(file_array[i]))
+    letter_frequencies = list(Counter(file_array[i]).values())
+    if 2 in letter_frequencies:
+        count_of_twos += 1
+    if 3 in letter_frequencies:
+        count_of_threes += 1
+
+pt1answer = count_of_twos * count_of_threes
+print(pt1answer)
