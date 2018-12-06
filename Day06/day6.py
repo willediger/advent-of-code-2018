feaa07a79f8666453ae3950553cb66556fdd0684
will Ediger <will.ediger@gmail.com>
2018-12-06 14:15:36 -0600

    day6 part 1 solution

--- Day06/day6.py
+++ Day06/day6.py
@@ -1,7 +1,7 @@
 from collections import Counter
 import re
 
-file_input = open('Day06/test_input', 'r')
+file_input = open('Day06/input', 'r')
 file_array = file_input.read().splitlines()
 file_input.close()
 
@@ -17,15 +17,35 @@ for y in range(0, max_loc[1] + 1):
 def distance(loc1, loc2):
     return abs(loc2[0] - loc1[0]) + abs(loc2[1] - loc1[1])
 
-
-for x in range(0, len(grid[0])):
-    for y in range(0, len(grid)):
+non_unique = -1
+grid_height = len(grid)
+grid_width = len(grid[0])
+for x in range(0, grid_width):
+    for y in range(0, grid_height):
         location_distances = [distance([x,y], loc) for loc in locations]
         min_distance = min(location_distances)
         if location_distances.count(min_distance) == 1:
             grid[y][x] = location_distances.index(min_distance)
         else:
-            grid[y][x] = -1
+            grid[y][x] = non_unique
+
+#add all elements in first row to infinite elements set
+infinite_elements = set([e for e in grid[0]])
+
+#add all elements in last row to infinite elements set
+infinite_elements.update([e for e in grid[len(grid)-1]])
+
+#add all elements in far left or far right to infinite elements set
+outside_elements = [[e[0], e[len(grid[0])-1]] for e in grid]
+outside_elements = [item for sublist in outside_elements for item in sublist]
+infinite_elements.update(outside_elements)
 
 
-breakpoint()
+flat_grid = [item for sublist in grid for item in sublist]
+
+area_counts = Counter(flat_grid)
+area_counts_without_infinite = {x: area_counts[x] for x in area_counts if x not in infinite_elements}
+
+max_area = max(area_counts_without_infinite.values())
+
+print('a', max_area)
